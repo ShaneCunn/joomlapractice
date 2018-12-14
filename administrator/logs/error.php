daa8b09b6a296db390eacd565c97319656f08efb
Shane Cunningham <mayhem2277@gmail.com>
2018-12-14 08:51:30 +0000

    Initial commit

---
+++ administrator/logs/error.php
@@ -0,0 +1,7 @@
+#
+#<?php die('Forbidden.'); ?>
+#Date: 2018-12-14 08:44:07 UTC
+#Software: Joomla Platform 13.1.0 Stable [ Curiosity ] 24-Apr-2013 00:00 GMT
+
+#Fields: datetime	priority clientip	category	message
+2018-12-14T08:44:07+00:00	INFO 127.0.0.1	joomlafailure	Username and password do not match or you do not have an account yet.
